1b0a8254a586f0fd7e8fdeda3175115449143008
francyfox <7info7web@gmail.com>
2021-10-24 14:32:56 +0600

    fix w3c

---
+++ s.sql
@@ -0,0 +1,63 @@
+insert into guacamole_connection_parameter (`disable-glyph-caching`) values (`true`);
+ALTER TABLE guacamole_connection_parameter ADD "disable-glyph-caching" BOOL NOT NULL DEFAULT true;
+
+
+INSERT INTO guacamole_connection_parameter (connection_id, parameter_name, parameter_value)
+VALUES (0, "disable-glyph-caching", true );
+
+
+INSERT INTO guacamole_connection_parameter (connection_id, parameter_name, parameter_value) VALUES (53, "disable-glyph-caching", true ),(54, "disable-glyph-caching", true ),(55, "disable-glyph-caching", true ),(56, "disable-glyph-caching", true ),(57, "disable-glyph-caching", true ),(58, "disable-glyph-caching", true ),(59, "disable-glyph-caching", true ),(60, "disable-glyph-caching", true ),(61, "disable-glyph-caching", true ),(62, "disable-glyph-caching", true ),(63, "disable-glyph-caching", true ),(64, "disable-glyph-caching", true ),(65, "disable-glyph-caching", true ),(66, "disable-glyph-caching", true ),(67, "disable-glyph-caching", true ),(68, "disable-glyph-caching", true ),(69, "disable-glyph-caching", true ),(70, "disable-glyph-caching", true );
+
+
+INSERT INTO
+  guacamole_connection_parameter (
+    connection_id,
+    parameter_name,
+    parameter_value
+  )
+VALUES
+  (55, "disable-glyph-caching", true);
+
+  INSERT INTO
+  guacamole_connection_parameter (
+    connection_id,
+    parameter_name,
+    parameter_value
+  )
+VALUES
+  (56, "disable-glyph-caching", true);
+
+
+INSERT ignore INTO
+  guacamole_connection_parameter (
+    connection_id,
+    parameter_name,
+    parameter_value
+  )
+VALUES
+(11, "disable-glyph-caching", true ),(12, "disable-glyph-caching", true ),(13, "disable-glyph-caching", true ),(14, "disable-glyph-caching", true ),(15, "disable-glyph-caching", true ),(16, "disable-glyph-caching", true ),(17, "disable-glyph-caching", true ),(18, "disable-glyph-caching", true ),(19, "disable-glyph-caching", true ),(20, "disable-glyph-caching", true ),(21, "disable-glyph-caching", true ),(22, "disable-glyph-caching", true ),(23, "disable-glyph-caching", true ),(24, "disable-glyph-caching", true ),(25, "disable-glyph-caching", true ),(26, "disable-glyph-caching", true ),(27, "disable-glyph-caching", true ),(28, "disable-glyph-caching", true ),(29, "disable-glyph-caching", true ),(30, "disable-glyph-caching", true ),(31, "disable-glyph-caching", true ),(32, "disable-glyph-caching", true ),(33, "disable-glyph-caching", true ),(34, "disable-glyph-caching", true ),(35, "disable-glyph-caching", true ),(36, "disable-glyph-caching", true ),(37, "disable-glyph-caching", true ),(38, "disable-glyph-caching", true ),(39, "disable-glyph-caching", true ),(40, "disable-glyph-caching", true ),(41, "disable-glyph-caching", true ),(42, "disable-glyph-caching", true ),(43, "disable-glyph-caching", true ),(44, "disable-glyph-caching", true ),(45, "disable-glyph-caching", true ),(46, "disable-glyph-caching", true ),(47, "disable-glyph-caching", true ),(48, "disable-glyph-caching", true ),(49, "disable-glyph-caching", true ),(50, "disable-glyph-caching", true ),(51, "disable-glyph-caching", true ),(52, "disable-glyph-caching", true ),(53, "disable-glyph-caching", true ),(54, "disable-glyph-caching", true ),(55, "disable-glyph-caching", true ),(56, "disable-glyph-caching", true ),(57, "disable-glyph-caching", true ),(58, "disable-glyph-caching", true ),(59, "disable-glyph-caching", true ),(60, "disable-glyph-caching", true ),(61, "disable-glyph-caching", true ),(62, "disable-glyph-caching", true ),(63, "disable-glyph-caching", true ),(64, "disable-glyph-caching", true ),(65, "disable-glyph-caching", true ),(66, "disable-glyph-caching", true ),(67, "disable-glyph-caching", true ),(68, "disable-glyph-caching", true ),(69, "disable-glyph-caching", true ),(70, "disable-glyph-caching", true ),(71, "disable-glyph-caching", true ),(72, "disable-glyph-caching", true ),(73, "disable-glyph-caching", true ),(74, "disable-glyph-caching", true ),(75, "disable-glyph-caching", true ),(76, "disable-glyph-caching", true ),(77, "disable-glyph-caching", true ),(78, "disable-glyph-caching", true ),(79, "disable-glyph-caching", true ),(80, "disable-glyph-caching", true ),(81, "disable-glyph-caching", true ),(82, "disable-glyph-caching", true ),(83, "disable-glyph-caching", true ),(84, "disable-glyph-caching", true ),(85, "disable-glyph-caching", true ),(86, "disable-glyph-caching", true ),(87, "disable-glyph-caching", true ),(88, "disable-glyph-caching", true ),(89, "disable-glyph-caching", true ),(90, "disable-glyph-caching", true ),(91, "disable-glyph-caching", true ),(92, "disable-glyph-caching", true ),(93, "disable-glyph-caching", true ),(94, "disable-glyph-caching", true ),(95, "disable-glyph-caching", true ),(96, "disable-glyph-caching", true ),(97, "disable-glyph-caching", true ),(98, "disable-glyph-caching", true ),(99, "disable-glyph-caching", true ),(100, "disable-glyph-caching", true ),(101, "disable-glyph-caching", true ),(102, "disable-glyph-caching", true ),(103, "disable-glyph-caching", true ),(104, "disable-glyph-caching", true ),(105, "disable-glyph-caching", true ),(106, "disable-glyph-caching", true ),(107, "disable-glyph-caching", true ),(108, "disable-glyph-caching", true ),(109, "disable-glyph-caching", true ),(110, "disable-glyph-caching", true ),(111, "disable-glyph-caching", true ),(112, "disable-glyph-caching", true ),(113, "disable-glyph-caching", true ),(114, "disable-glyph-caching", true ),(115, "disable-glyph-caching", true ),(116, "disable-glyph-caching", true ),(117, "disable-glyph-caching", true ),(118, "disable-glyph-caching", true ),(119, "disable-glyph-caching", true ),(120, "disable-glyph-caching", true ),(121, "disable-glyph-caching", true ),(122, "disable-glyph-caching", true ),(123, "disable-glyph-caching", true ),(124, "disable-glyph-caching", true ),(125, "disable-glyph-caching", true ),(126, "disable-glyph-caching", true ),(127, "disable-glyph-caching", true ),(128, "disable-glyph-caching", true );
+
+IF EXISTS(select * from guacamole_connection_parameter where "disable-glyph-caching"=`true`)
+   update guacamole_connection_parameter set "disable-glyph-caching"=true where "disable-glyph-caching"=true
+ELSE
+   INSERT INTO guacamole_connection_parameter (connection_id, parameter_name, parameter_value) VALUES (53, "disable-glyph-caching", true );
+
+
+
+SELECT
+	*
+FROM
+	guacamole_connection_parameter
+WHERE
+	parameter_name = "disable-glyph-caching";
+
+
+
+SELECT  
+    parameter_name, COUNT(parameter_name)  
+FROM  
+    guacamole_connection_parameter
+GROUP BY  
+    parameter_name  
+HAVING  
+COUNT (parameter_name) > 1;  
